17e1c9803b73fe05d6fb229984c08b97a79100a6
MiriamCall <bow18005@byui.edu>
2025-02-25 20:44:42 -0700

    Update navigation links and enhance message display area

--- src/utils/index.js
+++ src/utils/index.js
@@ -14,8 +14,8 @@ const getNav = async () => {
         <li><a href="/category/add">Add Category</a></li>
         <li><a href="/category/delete">Delete Category</a></li>
         <li><a href="/About">About Me</a></li>
-        <li><a href="/login">Login</a></li>
-        <li></li><a href="/register">Register</a></li>
+        <li><a href="/account/login">Login</a></li>
+        <li></li><a href="/account/register">Register</a></li>
         </ul>
     </nav>`;
 };
